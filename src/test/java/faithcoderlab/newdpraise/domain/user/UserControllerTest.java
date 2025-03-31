82f87cf465dc1a6eecce9d5e1b5208de1ba275c4
FaithCoderLab <seinplan97@gmail.com>
2025-03-31 14:27:50 +0900

    fix: UserControllerTest 테스트 환경 설정 오류 수정
    - UserControllerTest에 @ActiveProfiles('test') 어노테이션 추가
    - @Import(TestConfig.class) 추가
    
    Issue: #3

--- src/test/java/faithcoderlab/newdpraise/domain/user/UserControllerTest.java
+++ src/test/java/faithcoderlab/newdpraise/domain/user/UserControllerTest.java
@@ -9,6 +9,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import faithcoderlab.newdpraise.config.TestConfig;
 import faithcoderlab.newdpraise.domain.user.dto.SignupRequest;
 import faithcoderlab.newdpraise.domain.user.dto.SignupResponse;
 import java.time.LocalDateTime;
@@ -18,11 +19,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Import(TestConfig.class)
 class UserControllerTest {
 
   @Autowired
